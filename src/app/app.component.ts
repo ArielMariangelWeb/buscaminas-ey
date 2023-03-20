f60733a6a0998e5e39a44d332aa3b63f22e5fe8c
Ariel Mariangel <arielmariangelweb@gmail.com>
2023-03-20 20:39:28 -0300

    Se actualiza ganador y se deshabilitan botones al ganar o perder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,17 @@ export class AppComponent {
   count!: number;
   display: boolean = false;
   happy: boolean = true;
+  unlock: number = 0;
+  win: boolean = false;
+  disabled: boolean = false;
 
   ngOnInit(): void {
     this.display = false;
     this.isBomb = false;
     this.happy = true;
+    this.unlock = 0;
+    this.win = false;
+    this.disabled = false;
     for (let i = 0; i < this.squares.length; i++) {
       this.squares[i] = new Array(10);
     }
@@ -39,18 +45,20 @@ export class AppComponent {
   boom(square: string, v: number, h:number){
 
     this.display = true;
+    this.won;
     if(square === 'b'){
       this.isBomb = true;
       this.happy = false;
+      this.disabled = true;
+    } else {
+      this.numbersBomb(v, h);
     }
-
-    this.numbersBomb(v, h);
-
+    
+    
   }
-
+  
   numbersBomb(v: number, h:number){
     this.count = 0;
-
     for (let i = 0; i < this.squares.length; i++) {
       for (let j = 0; j < this.squares[i].length; j++) {
         if(this.squares[i][j] === 'b'){
@@ -60,34 +68,45 @@ export class AppComponent {
         }
       }
     }
-
+    
     if(this.squares[v][h] !== 'b'){
       if(this.count === 0){
-        this.openZero(v, h);
+        this.openZero(v, h); 
       } else {
+        if (this.count <= 1) {
+          this.unlock++
+        }
+        this.won();
         this.squares[v][h] = this.count;
       }
     }
   }
-
+  
   reset(){
     this.ngOnInit();
   }
-
+  
   openZero(v: number, h:number){
     this.squares[v][h] = 0;
-
+    
     for(let i = -1; i <= 1; i++){
       for(let j = -1; j <= 1; j++){
         const row = v + i;
         const col = h + j;
-
+        
         if(row >= 0 && col >= 0 && row < this.squares.length && col < this.squares[row].length){
           if(this.squares[row][col] !== 'b' && this.squares[row][col] !== 0){
             this.numbersBomb(row, col);
           }
         }
       }
+    }
+  }
+
+  won(){
+    if(this.unlock >= 90){
+      this.win = true;
+      this.disabled = true;
     }
   }
 }
